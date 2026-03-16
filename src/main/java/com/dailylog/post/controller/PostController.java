eccf52a7b92fe6871d61e406c7748b3d8716b858
HyeonGi2 <tmskc56@gmail.com>
2026-03-16 18:08:24 +0900

    타임리프 > REST API+React/Vue로 변경

--- src/main/java/com/dailylog/post/controller/PostController.java
+++ src/main/java/com/dailylog/post/controller/PostController.java
@@ -1,16 +1,20 @@
+/**
+* 타임리프 방식이 아닌 실무표준으로 변경 -> json, REST API + React/Vue
+* @Controller (HTMl 파일명 반환) -> @RestController (데이터 반환)
+* */
+
 package com.dailylog.post.controller;
 
 import com.dailylog.post.domain.Post;
+import com.dailylog.post.dto.PostCreateRequest;
 import com.dailylog.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+/*
 @RequiredArgsConstructor
 @Controller //
 public class PostController {
@@ -36,5 +40,33 @@ public class PostController {
     public String create(@RequestParam String title, @RequestParam String content) {
         postService.write(title, content); // 입력받은 제목과 내용 서비스로 이동시켜 DB에 저장
         return "redirect:/"; // 저장 완료 후, 메인화면으로 이동
+    } */
+
+
+@RestController // 중요. HTML 파일명이 아닌, 데이터(JSON) 반환.
+@RequiredArgsConstructor
+@RequestMapping("/api/posts") // 이 컨트롤러의 모든 주소는 /api/posts로 시작
+public class PostController {
+
+    private final PostService postService;
+
+    /**
+    모든 게시글 목록 조회
+    접근 주소: GET 8081/api/posts
+    */
+    @GetMapping
+    public List<Post> getPosts() {
+        // model 사용 x, db에서 찾은 리스트를 바로 출력
+        // 스프링부트 내에서 데이터를 json 형태로 변환, 프론트엔드 전달
+        return postService.findPosts();
+    }
+
+    /**
+    * 접근 주소: POST 8081/api/posts
+    * */
+    @PostMapping
+    public Long createPost(@RequestBody PostCreateRequest request) {
+        // @RequestBody: 프론트엔드가 보내온 json을 dto 객체와 맵핑
+        return postService.write(request.getTitle(), request.getContent());
     }
 }
---
+++ src/main/java/com/dailylog/post/dto/PostCreateRequest.java
@@ -0,0 +1,10 @@
+package com.dailylog.post.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PostCreateRequest {
+    //
+    private String title;
+    private String content;
+}
